6d54e1dd5aa9726d4eecff527421fcd25a8a5c1d
yuriaguiar-86 <kakacraft337@gmail.com>
2026-02-02 20:45:18 -0300

    (remove) efetuado o deslocamento do mapeamento de rota api/version para o index

--- src/router/operation-route.js
+++ src/router/operation-route.js
@@ -2,10 +2,10 @@ const express = require('express');
 const operationController = require('../controller/operation-controller');
 const operationRouter = express.Router();
 
-operationRouter.get('/api/v1/operations', operationController.index);
-operationRouter.get('/api/v1/operations/:id', operationController.view);
-operationRouter.post('/api/v1/operations', operationController.add);
-operationRouter.put('/api/v1/operations/:id', operationController.edit);
-operationRouter.delete('/api/v1/operations/:id', operationController.delete);
+operationRouter.get('/operations', operationController.index);
+operationRouter.get('/operations/:id', operationController.view);
+operationRouter.post('/operations', operationController.add);
+operationRouter.put('/operations/:id', operationController.edit);
+operationRouter.delete('/operations/:id', operationController.delete);
 
 module.exports = operationRouter;

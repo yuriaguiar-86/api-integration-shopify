66331ab55be3412d7b8c52b328e95eb5ed25623d
yuriaguiar-86 <kakacraft337@gmail.com>
2026-02-02 20:42:02 -0300

    (feat) acesso da port de run alterado para env

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const connection = require('./src/model/connection');
 const operationRouter = require('./src/router/operation-route');
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(operationRouter);
+app.use('/api/v1', operationRouter);
 
 connection.sequelize.sync({ force: true })
     .then(() => {
@@ -18,59 +18,3 @@ connection.sequelize.sync({ force: true })
     .catch((error) => {
         console.log(`Não foi possível conectar ao banco de dados: ${error}`);
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const Shopify = require('shopify-api-node');
-
-// const shopify = new Shopify({
-//   shopName: process.env.SHOP_NAME,
-//   apiKey: process.env.API_KEY,
-//   password: process.env.API_SECRET,
-// });
-
-
-// app.get('/', (req, res) => {
-//     res.send('<h1>Shopify with NODEJS</h1>');
-// });
-
-// app.get('/products', async (req, res) => {
-//     try {
-//         await shopify.product
-//             .list({ limit: 100 })
-//             .then((products) => res.send(products))
-//             .catch((err) => console.error(err));
-
-//     } catch (err) {
-//         console.error('Error:', err.response.data);
-//         res.status(500).send('Failed to fetch products');
-//     }
-// });
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-// });

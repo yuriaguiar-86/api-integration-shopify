6abafaa0ae5b19f3e56a76876f1a3f64ccf9c90f
yuriaguiar-86 <kakacraft337@gmail.com>
2026-02-03 02:05:44 -0300

    (feat) adicionado rota da shopify

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const connection = require('./src/model/connection');
 const operationRouter = require('./src/router/operation-route');
+const shopifyRouter = require('./src/router/shopify-route');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/api/v1', operationRouter);
+app.use('/api/v1', shopifyRouter);
 
 connection.sequelize.sync({ force: true })
     .then(() => {
